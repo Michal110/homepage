1adb17dcbc899c5ded42538198957607ffa35512
Michał Bąk <michaelbak@op.pl>
2020-04-20 21:39:16 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('siema ');
+const name = 'Michał Bąk';
+const age = 31;
+const maciek = null;
+console.log (name);
+console.log(age);
+console.log(`${name}
+${age}`);
+console.log(`Nazywam się ${name} i mam ${age} lat`);
